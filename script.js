c065fdc13f5b6deabf068ded963c8df5b12720f1
Vladyslav-Karpenko <vladyslav.karpenko@ukr.net>
2025-10-05 23:09:46 +0200

    homework finished

---
+++ script.js
@@ -0,0 +1,10 @@
+const modal = document.getElementById("modal");
+const openModal = document.getElementById("openModal");
+const closeModal = document.getElementById("closeModal");
+
+openModal.onclick = () => (modal.style.display = "flex");
+closeModal.onclick = () => (modal.style.display = "none");
+
+const hamburger = document.getElementById("hamburger");
+const menu = document.querySelector(".navList");
+hamburger.onclick = () => menu.classList.toggle("open");
